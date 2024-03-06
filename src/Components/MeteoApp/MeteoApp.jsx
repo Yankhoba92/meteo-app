30c2d6843a8b661e63b0496ad9340230b03d5345
Yankhoba <bagayoko.yan@gmail.com>
2024-03-06 15:34:48 +0100

    récupération et affichage de notre api

--- src/Components/MeteoApp/MeteoApp.jsx
+++ src/Components/MeteoApp/MeteoApp.jsx
@@ -10,12 +10,35 @@ import wind_icon from "../Assets/wind.png"
 import humidity_icon from "../Assets/humidity.png"
 
 
+
 const MeteoApp = () => {
+  let api_key = "175383884ca509f69c6b3c84b19580f3";
+  const search = async ()=>{
+    const element = document.getElementsByClassName("cityInput")
+    if(element[0].value === "")
+    {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent")
+    const wind = document.getElementsByClassName("wind-rate")
+    const temperature = document.getElementsByClassName("meteo-temps")
+    const position = document.getElementsByClassName("meteo-position")
+
+    humidity[0].innerHTML = data.main.humidity+" %";
+    wind[0].innerHTML = data.wind.speed+" km/h";
+    temperature[0].innerHTML = data.main.temp+"°c";
+    position[0].innerHTML = data.name;
+
+  }
   return (
     <div className='container'>
       <div className="top-bar">
         <input type="text" className='cityInput' placeholder='search' />
-        <div className="search-icon">
+        <div className="search-icon" onClick={()=>{search()}}>
           <img src={search_icon} alt="" />
         </div>
       </div>
@@ -35,7 +58,7 @@ const MeteoApp = () => {
         <div className="element">
           <img src={wind_icon} alt="" />
           <div className="data">
-            <div className="humidity-percent">18km/h</div>
+            <div className="wind-rate">18km/h</div>
             <div className="text">vent</div>
           </div>
         </div>
