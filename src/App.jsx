88d36584f5daf9c43ea668d9ba0a636dc6899b61
Yankhoba <bagayoko.yan@gmail.com>
2024-03-06 14:51:56 +0100

    🚚 importation de mon component MeteoApp

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MeteoApp from './Components/MeteoApp/MeteoApp'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+    <div className=''>
+      <MeteoApp/>
+    </div>
        
     </>
   )
