a9f205e0f6c4b355e7f8c5e1f008bfe0f165a775
Yankhoba <bagayoko.yan@gmail.com>
2024-03-06 15:50:23 +0100

    icon de temps

--- src/Components/MeteoApp/MeteoApp.jsx
+++ src/Components/MeteoApp/MeteoApp.jsx
@@ -1,49 +1,69 @@
-import React from 'react'
-import './MeteoApp.css'
-import search_icon from "../Assets/search.png"
-import clear_icon from "../Assets/clear.png"
-import cloud_icon from "../Assets/cloud.png"
-import drizzle_icon from "../Assets/drizzle.png"
-import rain_icon from "../Assets/rain.png"
-import snow_icon from "../Assets/snow.png"
-import wind_icon from "../Assets/wind.png"
-import humidity_icon from "../Assets/humidity.png"
-
-
+import React, { useState } from "react";
+import "./MeteoApp.css";
+import search_icon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import cloud_icon from "../Assets/cloud.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import rain_icon from "../Assets/rain.png";
+import snow_icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+import humidity_icon from "../Assets/humidity.png";
 
 const MeteoApp = () => {
   let api_key = "175383884ca509f69c6b3c84b19580f3";
-  const search = async ()=>{
-    const element = document.getElementsByClassName("cityInput")
-    if(element[0].value === "")
-    {
+  const [wicon, setWicon] = useState(cloud_icon);
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
       return 0;
     }
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
 
     let response = await fetch(url);
     let data = await response.json();
-    const humidity = document.getElementsByClassName("humidity-percent")
-    const wind = document.getElementsByClassName("wind-rate")
-    const temperature = document.getElementsByClassName("meteo-temps")
-    const position = document.getElementsByClassName("meteo-position")
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temperature = document.getElementsByClassName("meteo-temps");
+    const position = document.getElementsByClassName("meteo-position");
 
-    humidity[0].innerHTML = data.main.humidity+" %";
-    wind[0].innerHTML = data.wind.speed+" km/h";
-    temperature[0].innerHTML = data.main.temp+"°c";
+    humidity[0].innerHTML = data.main.humidity + " %";
+    wind[0].innerHTML = data.wind.speed + " km/h";
+    temperature[0].innerHTML = data.main.temp + "°c";
     position[0].innerHTML = data.name;
 
-  }
+    if (data.wether[0].icon === "01d" || data.wether[0].icon === "01n") {
+      setWicon(clear_icon);
+    } else if (data.wether[0].icon === "02d" || data.wether[0].icon === "02n") {
+      setWicon(cloud_icon);
+    } else if (data.wether[0].icon === "03d" || data.wether[0].icon === "03n") {
+      setWicon(drizzle_icon);
+    } else if (data.wether[0].icon === "04d" || data.wether[0].icon === "04n") {
+      setWicon(cloud_icon);
+    } else if (data.wether[0].icon === "09d" || data.wether[0].icon === "09n") {
+      setWicon(rain_icon);
+    } else if (data.wether[0].icon === "10d" || data.wether[0].icon === "10n") {
+      setWicon(rain_icon);
+    } else if (data.wether[0].icon === "13d" || data.wether[0].icon === "13n") {
+      setWicon(snow_icon);
+    } else {
+      setWicon(clear_icon);
+    }
+  };
   return (
-    <div className='container'>
+    <div className="container">
       <div className="top-bar">
-        <input type="text" className='cityInput' placeholder='search' />
-        <div className="search-icon" onClick={()=>{search()}}>
+        <input type="text" className="cityInput" placeholder="search" />
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
           <img src={search_icon} alt="" />
         </div>
       </div>
-      <div className='meteo-image'>
-        <img src={cloud_icon} alt="" />
+      <div className="meteo-image">
+        <img src={wicon} alt="" />
       </div>
       <div className="meteo-temps">24°c</div>
       <div className="meteo-position">London</div>
@@ -64,7 +84,7 @@ const MeteoApp = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MeteoApp
+export default MeteoApp;
