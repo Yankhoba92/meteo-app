2bcbaffada8ea620cbef892e8b100603539e36c7
Yankhoba <bagayoko.yan@gmail.com>
2024-03-06 14:53:12 +0100

    🚧 conception de notre default page

--- src/Components/MeteoApp/MeteoApp.jsx
+++ src/Components/MeteoApp/MeteoApp.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
-import './MetoApp.css'
+import './MeteoApp.css'
+import search_icon from "../Assets/search.png"
+import clear_icon from "../Assets/clear.png"
+import cloud_icon from "../Assets/cloud.png"
+import drizzle_icon from "../Assets/drizzle.png"
+import rain_icon from "../Assets/rain.png"
+import snow_icon from "../Assets/snow.png"
+import wind_icon from "../Assets/wind.png"
+import humidity_icon from "../Assets/humidity.png"
 
-const MetoApp = () => {
+
+const MeteoApp = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="top-bar">
+        <input type="text" className='cityInput' placeholder='search' />
+        <div className="search-icon">
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className='meteo-image'>
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="meteo-temps">24°c</div>
+      <div className="meteo-position">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src="" alt="" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src="" alt="" />
+          <div className="data">
+            <div className="humidity-percent">18km/h</div>
+            <div className="text">vent</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default MetoApp
+export default MeteoApp
